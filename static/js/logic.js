4d4079a3b77e6c76ae3d27fcdee537b653e5122d
Jason Engel <engel.jason@gmail.com>
2021-09-05 21:14:38 -0700

    added legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,11 +18,34 @@ var overlays = {
     "Magnitude 1+": layers.ONE_PLUS
 };
 
-L.control.layers(null, overlays).addTo(map);
-
 var usgs_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson";
 
 const colors = ['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#b10026'];
+const ranges = [0, 5, 10, 15, 25, 50, 75, 100];
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'legend');
+    var from;
+    var to;
+    var labels = [];
+
+    labels.push('<i>Depth</i>')
+    for (var i = 0; i < ranges.length; i++) {
+        from = ranges[i];
+        to = ranges[i + 1];
+
+        labels.push(
+            '<i style="background:' + colors[i] + '"></i> ' +
+            from + (to ? '&ndash;' + to : '+'));
+    }
+
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
+legend.addTo(map);
+
 
 
 d3.json(usgs_url).then(function(featureCollection) {
@@ -31,13 +54,13 @@ d3.json(usgs_url).then(function(featureCollection) {
         var feature = Object.assign({}, features[i]);
         var coord = feature.geometry.coordinates;
         var d = coord[2];
-        var color = d < 5 ? colors[0]:
-                    d < 10 ? colors[1]:
-                    d < 15 ? colors[2]:
-                    d < 25 ? colors[3]:
-                    d < 50 ? colors[4]:
-                    d < 75 ? colors[5]:
-                    d < 100 ? colors[6]:
+        var color = d < ranges[1] ? colors[0]:
+                    d < ranges[2] ? colors[1]:
+                    d < ranges[3] ? colors[2]:
+                    d < ranges[7] ? colors[3]:
+                    d < ranges[5] ? colors[4]:
+                    d < ranges[6] ? colors[5]:
+                    d < ranges[7] ? colors[6]:
                     colors[7];
         var mag = feature.properties.mag;
         var date = new Date(feature.properties.time).toDateString();
@@ -49,7 +72,7 @@ d3.json(usgs_url).then(function(featureCollection) {
             fillColor: color,
             fillOpacity: 0.5,
             radius: mag * 10000
-        }).bindPopup(tooltip).addTo(map); // TODO:  add legend
+        }).bindPopup(tooltip).addTo(map);
         
     }
     
